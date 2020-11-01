d9be3e006c7de7fa1a5ce1cfb5d3d435e33053b2
joshhunt <josh@trtr.co>
2020-11-01 16:45:15 +0000

    add cache plugin

---
+++ plugins/netlify-plugin-api-cache/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Restore file/directory cached in previous builds.
+  // Does not do anything if:
+  //  - the file/directory already exists locally
+  //  - the file/directory has not been cached yet
+  async onPreBuild({ utils }) {
+    const result = await utils.cache.restore("./api-cache");
+    console.log("onPreBuild utils.cache.restore result", result);
+
+    const files = await utils.cache.list();
+    console.log("onPreBuild Cached files", files);
+  },
+
+  // Cache file/directory for future builds.
+  // Does not do anything if:
+  //  - the file/directory does not exist locally
+  async onPostBuild({ utils }) {
+    const result = await utils.cache.save("./api-cache");
+
+    console.log("onPostBuild utils.cache.save result", result);
+  },
+};
