8872c7f4774a976acecef9e40cbec936918e7669
joshhunt <josh@trtr.co>
2020-11-01 16:56:19 +0000

    fix cache dir in plugin

--- plugins/netlify-plugin-api-cache/index.js
+++ plugins/netlify-plugin-api-cache/index.js
@@ -4,7 +4,7 @@ module.exports = {
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
-    const result = await utils.cache.restore("./api-cache");
+    const result = await utils.cache.restore("./.api-cache/shared");
     console.log("onPreBuild utils.cache.restore result", result);
 
     const files = await utils.cache.list();
@@ -15,7 +15,7 @@ module.exports = {
   // Does not do anything if:
   //  - the file/directory does not exist locally
   async onPostBuild({ utils }) {
-    const result = await utils.cache.save("./api-cache");
+    const result = await utils.cache.save("./.api-cache/shared");
 
     console.log("onPostBuild utils.cache.save result", result);
   },
