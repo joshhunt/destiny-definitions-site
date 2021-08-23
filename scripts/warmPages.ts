fe6b68ca02af2d6a23cd5ebafa7d795b1eba7199
joshhunt <josh@trtr.co>
2021-08-23 01:22:49 +0100

    fix warm script

--- scripts/warmPages.ts
+++ scripts/warmPages.ts
@@ -11,7 +11,7 @@ async function main() {
   let { data: versionsIndex } = await axios.get(
     "https://destiny-definitions.s3-eu-west-1.amazonaws.com/index.json"
   );
-  versionsIndex = versionsIndex.reverse().slice(0, 5);
+  versionsIndex = versionsIndex.reverse();
 
   const urls: CacheEntry[] = [];
 
@@ -37,20 +37,16 @@ async function main() {
       .filter((s) => Object.values(s[1]).some((v) => v.length))
       .map((v) => v[0]);
 
-    console.log(tables);
-
     for (const tableName of tables) {
       urls.push({
-        url: `http://localhost:81/version/${id}/${tableName}`,
+        url: `https://archive.destiny.report/version/${id}/${tableName}`,
         id,
         tableName,
       });
     }
   }
 
-  console.log(urls);
   asyncLib.eachLimit(urls, 2, ({ url, id, tableName }: CacheEntry, cb: any) => {
-    console.log("requesting", url, cb);
     axios
       .get(url)
       .then((resp: any) => {
