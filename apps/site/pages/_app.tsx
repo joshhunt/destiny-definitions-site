27f209179d98055fb1b5d627bd3e4af773ceaba5
Josh Hunt <joshhunt@users.noreply.github.com>
2023-02-15 17:04:24 +0100

    tweak objectives

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -11,7 +11,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import "./common.scss";
 
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import { Breadcrumb } from "../components/Breadcrumbs";
 
